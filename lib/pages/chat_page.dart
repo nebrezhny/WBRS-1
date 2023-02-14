afec4940c414fc9dee4831680a8224578b65f585
Dmitriev Denis <den.dmitriev.2017@inbox.ru>
2023-02-14 18:52:21 +0300

    new

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -77,7 +77,7 @@ class _ChatPageState extends State<ChatPage> {
       sound: true,
     );
 
-    firebaseMessaging.getNotificationSettings(IosNotificationsSettings());
+    firebaseMessaging.getNotificationSettings();
 
     print(settings.authorizationStatus);
   }
